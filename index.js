a2daa73810bf0c3da7d71bb462266f86455c3ff4
DiceMast3r <babyisbap@gmail.com>
2021-11-05 04:50:36 +0700

    formatted

--- index.js
+++ index.js
@@ -1,93 +1,84 @@
-const {Client, Intents} = require('discord.js');
-const config = require('./config.json')
+const { Client, Intents } = require("discord.js");
+const config = require("./config.json");
 //const dotenv = require('dotenv');
-const axios = require('axios'); 
-const prefix = "!"
+const axios = require("axios");
+const prefix = "!";
 
 //dotenv.config();
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
-const client = new Client(
-    {
-        intents: [
-            Intents.FLAGS.GUILDS,
-            Intents.FLAGS.GUILD_MESSAGES,
-        ]
-    }
-);
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
-client.on('ready', () => {
-    console.log("Bot ready");
+client.on("ready", () => {
+  console.log("Bot ready");
 });
 
 async function getMeme() {
-    const res = await axios.get('https://meme-api.herokuapp.com/gimme')
-    return null
+  const res = await axios.get("https://meme-api.herokuapp.com/gimme");
+  return null;
 }
 
-const food = []
+const food = [];
 
-client.on('messageCreate', async msg => {
-    if (msg.content == "!help") {
-        msg.reply("!info = แสดงสถานะ server\n!list = แสดงรายชื่อข้าว\n!food = สุ่มข้าว\n!addf <ชื่อข้าว> = เพิ่มข้าวลงในรายชื่อ\n!clearfood = ล้างรายชื่อข้าว\n!memes = ขอมีม")
+client.on("messageCreate", async (msg) => {
+  if (msg.content == "!help") {
+    msg.reply(
+      "!info = แสดงสถานะ server\n!list = แสดงรายชื่อข้าว\n!food = สุ่มข้าว\n!addf <ชื่อข้าว> = เพิ่มข้าวลงในรายชื่อ\n!clearfood = ล้างรายชื่อข้าว\n!memes = ขอมีม"
+    );
+  } else if (msg.content == "!memes") {
+    const meme = await axios.get("https://meme-api.herokuapp.com/gimme");
+    const img2 = meme.data.url;
+    msg.reply(img2);
+  } else if (msg.content == "!info") {
+    msg.reply("ชื่อเซิร์ฟเวอร์: " + msg.guild.name);
+    msg.reply("สมาชิกทั้งหมด: " + msg.guild.memberCount + " คน");
+  } else if (msg.content == "!list") {
+    if (food.length == 0) {
+      msg.reply("รายชื่อว่าง");
+    } else {
+      msg.reply(food.toString());
     }
-    else if (msg.content == "!memes") {
-        const meme = await axios.get('https://meme-api.herokuapp.com/gimme')
-        const img2 = meme.data.url;
-        msg.reply(img2);
-    }
-    else if (msg.content == "!info") {
-        msg.reply("ชื่อเซิร์ฟเวอร์: " + msg.guild.name);
-        msg.reply("สมาชิกทั้งหมด: "+ msg.guild.memberCount + " คน");
-    }
-    else if (msg.content == "!list") {
-        if (food.length == 0) {
-            msg.reply("รายชื่อว่าง")
-        }
-        else {
-            msg.reply(food.toString());
-        }
-    }
-})
+  }
+});
 
-client.on('messageCreate', async msg => {
-    if (!msg.content.startsWith(prefix)) return;
-    const withoutPrefix = msg.content.slice(prefix.length);
-	const split = withoutPrefix.split(/ +/);
-	const command = split[0];
-	const args = split.slice(1);
-    if(command == "addf") {
-        for (var i = 0; i < args.length; i++){
-            food.push(args[i]);
-        }
-        console.log(food)
-        msg.reply("เพิ่ม " +"'" + args.join(', ') + "'" + " ลงในรายชื่อ")
+client.on("messageCreate", async (msg) => {
+  if (!msg.content.startsWith(prefix)) return;
+  const withoutPrefix = msg.content.slice(prefix.length);
+  const split = withoutPrefix.split(/ +/);
+  const command = split[0];
+  const args = split.slice(1);
+  if (command == "addf") {
+    for (var i = 0; i < args.length; i++) {
+      food.push(args[i]);
     }
-    else if (command == "food") {
-        if (food.length == 0) {
-            msg.reply("รายชื่อว่าง")
-        }
-        else {
-            var x = getRandomInt(0, food.length);
-            msg.reply(food[x])
-            console.log(food[x])    
-        }
+    console.log(food);
+    msg.reply("เพิ่ม " + "'" + args.join(", ") + "'" + " ลงในรายชื่อ");
+  } else if (command == "food") {
+    if (food.length == 0) {
+      msg.reply("รายชื่อว่าง");
+    } else {
+      var x = getRandomInt(0, food.length);
+      msg.reply(food[x]);
+      console.log(food[x]);
     }
-    else if (command == "clearfood") {
-        food.length = 0
-        console.log(food)
-        msg.reply("ล้างรายชื่อแล้ว")
-    }
-})
+  } else if (command == "clearfood") {
+    food.length = 0;
+    console.log(food);
+    msg.reply("ล้างรายชื่อแล้ว");
+  }
+});
 
-client.on('guildCreate', guild => {
-    guild.systemChannel.send("Hello I'm Raiden Bot!, Use !help to see command XD")
-})
-
+client.on("guildCreate", (guild) => {
+  guild.systemChannel.send(
+    "Hello I'm Raiden Bot!, Use !help to see command XD"
+  );
+});
 
 client.login(config.token);
