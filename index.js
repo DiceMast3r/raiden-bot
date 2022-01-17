ffed966a8c33039f83a0005067a050fff7c79a4c
DiceMast3r <babyisbap@gmail.com>
2022-01-18 01:37:20 +0700

    Format code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const prefix = "!";
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 const client = new Client({
